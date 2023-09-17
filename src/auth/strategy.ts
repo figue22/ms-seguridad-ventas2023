8d7f7bfcadf6c30040df33cbfd1a6a10bb2e06bf
Juan Manuel <juanmanuelfigueroavalencia@gmail.com>
2023-09-17 16:06:37 -0500

    Estrategia de Autorizacion LB4 (faltan verificaciones)

---
+++ src/auth/strategy.ts
@@ -0,0 +1,17 @@
+import {AuthenticationStrategy} from '@loopback/authentication';
+import {Request} from '@loopback/rest';
+import {UserProfile} from '@loopback/security';
+
+export class BasicAuthenticationStrategy implements AuthenticationStrategy {
+  name: string = 'auth';
+
+  constructor(
+  ) { }
+
+  async authenticate(request: Request): Promise<UserProfile | undefined> {
+
+    console.log("ejecutanto estrategia")
+
+    return undefined;
+  }
+}
