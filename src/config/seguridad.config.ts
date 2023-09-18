d828b3507b08ff9ac35720fb90616b0163d91aa2
Juan Manuel <juanmanuelfigueroavalencia@gmail.com>
2023-09-17 23:47:26 -0500

    Pruebas y ajustes de la estrategia de autorización

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,3 +1,9 @@
 export namespace ConfiguracionSeguridad {
   export const claveJWT: string = "Admin@2023#";
+  export const menuUsuarioId = "64fa3a7b0650a53a50a45fe9";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
 }
